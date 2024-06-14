b98f5b4b79dd9d11405f333b1743056fffe5341e
Suzanne Scott <sscott@cmri.org.au>
2024-06-14 15:37:44 +1000

    Script to make some plots for a PacBio comparison figure

--- scripts/sh2389_selection/plots.R
+++ scripts/sh2389_selection/plots.R
@@ -3,102 +3,11 @@
 library(tidyverse)
 library(cowplot)
 here::i_am("scripts/sh2389_selection/plots.R")
-
+source("scripts/sh2389_selection/helpers.R")
 out_dir <- here::here("out/sh2389/plots")
 dir.create(out_dir, showWarnings = FALSE, recursive = TRUE)
 
 ggplot2::theme_set(ggplot2::theme_minimal(base_size = 8))
-
-pos_num <- function(v) {
-    return(stringr::str_extract(v, "^\\d+"))
-}
-
-var_to_pos <- function(v) {
-    pos <- pos_num(v) %>% 
-        as.numeric()
-    pos_fct <- dplyr::case_when(
-        stringr::str_detect(v, "sub") ~ pos_num(v),
-        stringr::str_detect(v, "ins") ~ glue::glue("{pos_num(v)} "),
-        stringr::str_detect(v, "del") ~ glue::glue("{pos_num(v)}  ")
-    )
-    pos_fct <- forcats::fct_reorder(pos_fct, pos)
-    return(pos_fct)
-}
-
-get_n_rows <- function(filename) {
-    n_rows <- system(paste("zcat", filename, "| wc -l"), intern = TRUE)
-    return(as.numeric(n_rows)-1)
-
-}
-
-load_library <- function(base, n_reads=5000) {
-  
-    aav_names <- c("AAV2_N496D", "AAV3b", "AAV8", "AAV9", "ambiguous")
-    pal <- scales::hue_pal()(5)
-    names(pal) <- aav_names
-    
-    # read assigned parents and counts
-    assigned_file <- file.path("out", "parents", "counts", glue::glue("{base}_parent-counts.tsv.gz"))
-    assigned <- readr::read_tsv(file = assigned_file, n_max = n_reads) %>%
-    # reverse order and assign ids
-        dplyr::slice(n():1) %>%
-        dplyr::mutate(id = dplyr::row_number())
-
-    # total number of reads
-    n_reads <- get_n_rows(file.path("out", "parents", "assigned", glue::glue("{base}_assigned-parents.tsv.gz")))
-
-    # read breakpoints
-    breakpoints <- readr::read_tsv(file = file.path("out", "parents", "breaks", glue::glue("{base}-pervar.tsv.gz")))
-
-    p1 <- assigned %>%
-        # pivot longer
-        dplyr::select(-count) %>%
-        tidyr::pivot_longer(cols = -id, names_to = "variant", values_to = "parent") %>% 
-        # get position of each variant
-        dplyr::mutate(pos = var_to_pos(variant)) %>%
-        # for multiple parents, just put "ambiguous"
-        dplyr::mutate(parent = case_when(
-            stringr::str_detect(parent, ",") ~ "ambiguous",
-            TRUE ~ parent
-        )) %>%
-        ggplot2::ggplot(ggplot2::aes(x=pos, y=id, fill=parent)) + 
-            ggplot2::geom_raster() +
-        ggplot2::labs(x="Reference position", y="Read", fill="Parent") +
-        ggplot2::scale_fill_manual(values=pal) +
-        ggplot2::theme(axis.title = element_blank(), axis.text = element_blank())
-
-    legend <- cowplot::get_legend(p1)
-    p1 <- p1 + ggplot2::theme(legend.position="none") 
-
-
-    # counts
-    p2 <- assigned %>% 
-        ggplot2::ggplot(ggplot2::aes(x=count, y=id, group=1)) +
-        ggplot2::geom_point() +
-        ggplot2::geom_line() +
-        ggplot2::labs(x="Count", y="Read") +
-        ggplot2::scale_x_log10() +
-        ggplot2::theme(axis.text.x = element_text(angle=90, vjust=0.5, hjust=1))
-
-    # fraction of breakpoints
-    breakpoints <- breakpoints %>% 
-        dplyr::mutate(pos = var_to_pos(location)) %>%
-        dplyr::mutate(fraction = breakpoints / n_reads) 
-    p3 <- breakpoints %>%
-        ggplot2::ggplot(ggplot2::aes(x = pos, y=fraction)) +
-        ggplot2::geom_col() +
-        ggplot2::labs(x="Reference position", y="Read\nfraction") +
-        ggplot2::scale_x_discrete(breaks = levels(breakpoints$pos)[c(T, rep(F, 50))]) +
-        ggplot2::theme(axis.text.x = element_text(angle=90, vjust=0.5, hjust=1))
-
-    p <- cowplot::plot_grid(p2, p1, NULL, p3, 
-                nrow = 2, ncol=2,  rel_widths = c(0.3, 1), rel_heights=c(1, 0.3))
-    cowplot::plot_grid(p, legend, nrow=1, rel_widths=c(1, 0.1))
-
-    return(list(n_reads = n_reads, assigned=assigned, 
-                    breakpoints=breakpoints, 
-                    p1=p1, p2=p2, p3=p3, lgd=legend, p=p))
-}
 
 
 r0_vis <- load_library("r0_np-cc")
@@ -108,8 +17,12 @@ r1_vis$p
 r5_vis <- load_library("r5_np-cc")
 r5_vis$p
 
-# import counts of amino acid sequences
+# in r5 library, how many breakpoints over 0.5?
+print(glue::glue("Breakpoints in r5 with fraction > 0.5:"))
+r5_vis$breakpoints %>%
+  filter(fraction > 0.5)
 
+# import counts of amino acid sequences
 aa_files <- list.files(here::here("out", "corrected", "counts"), pattern = "aa-seq-counts.tsv.gz", full.names=TRUE)
 # load only np-cc data
 aa_files <- aa_files[stringr::str_detect(aa_files, "np-cc")]
@@ -268,3 +181,54 @@ ggplot2::ggsave(file.path(out_dir, "selection_rounds_r0_to_r1_frac.png"), plot =
 
 ggplot2::ggsave(file.path(out_dir, "selection_rounds_r0_to_r5_frac.pdf"), plot = p3, units = "mm", width = 55, height = 55, scale=1.5)
 ggplot2::ggsave(file.path(out_dir, "selection_rounds_r0_to_r5_frac.png"), plot = p3, units = "mm", width = 55, height = 55, scale=1.5, dpi = 300)
+
+
+#### read counts at each stage ####
+
+
+# read in data
+rc <- read_tsv(list.files(here::here("out", "qc"), pattern = "*read-counts.tsv", full.names = TRUE), 
+                    col_names = c("counted_file", "type", "count"), id = "count_file", show_col_types = F) 
+
+rc <- rc %>% 
+  rowwise() %>% 
+  mutate(sample = stringr::str_extract(basename(count_file), ".+(?=_read-counts.tsv)")) %>%
+  dplyr::mutate(stage = case_when(
+    stringr::str_detect(counted_file, "data") & type == "fastq" ~ "raw",
+    stringr::str_detect(sample, "sanger") & type == "fasta" ~ "raw",
+    stringr::str_detect(counted_file, "R2C2_Cons") ~ "consensus",
+    stringr::str_detect(counted_file, "c3poa_filt") ~ "filtered consensus",
+    type == "variant_tsv" ~ "variant",
+    type == "pivoted_tsv" ~ "pivoted",
+    type == "distinct_read_counts" & stringr::str_detect(counted_file, "aa-seq") ~ "distinct aa",
+    type == "distinct_read_counts" ~ "distinct nt",
+  )) %>% 
+  mutate(stage = factor(stage, levels = c("raw", "consensus", "filtered consensus", "variant", "pivoted", "distinct aa", "distinct nt"))) 
+
+
+# write to file
+rc %>% 
+  select(sample, stage, count) %>%
+  pivot_wider(names_from = stage, values_from = count) %>%
+  write_tsv(file.path(out_dir, "read_counts.tsv"))
+
+
+# also aggregate over same sample and sequncing type for tex
+rc_agg <- rc %>%
+  mutate(seq_type = stringr::str_extract(sample, "sanger|pb|np-cc|np")) %>%
+  mutate(sample_stage = stringr::str_extract(sample, "aav2|r0|r1|r5")) %>%
+  group_by(seq_type, sample_stage, stage) %>%
+  summarise(count = sum(count)) %>%
+  ungroup() 
+
+rc_agg  %>%
+  pivot_wider(names_from = stage, values_from = count) %>%
+  write_tsv(file.path(out_dir, "read_counts_agg.tsv"))
+
+# also caculate amount of reads at pivoted stage relative to raw
+rc_agg %>% 
+  filter(stage == "raw" | stage == "pivoted") %>%
+  pivot_wider(names_from = stage, values_from = count) %>%
+  mutate(pivoted_div_raw = pivoted / raw) %>%
+  write_tsv(file.path(out_dir, "read_counts_agg_pivoted.tsv"))
+
